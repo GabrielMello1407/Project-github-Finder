5f33f2ca757b5e8d93062044b2090808f20df948
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2023-10-10 17:22:17 -0300

    github_finder check

---
+++ github_finder/src/components/BackBtn.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from 'react-router-dom'
+import classes from './BackBtn.module.css'
+const BackBtn = () => {
+  const navigate = useNavigate()
+  return (
+    <>
+      <button onClick={() => navigate(-1)} className={classes.back_btn}>Voltar</button></>
+  )
+}
+
+export default BackBtn
